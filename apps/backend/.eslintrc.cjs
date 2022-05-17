4adcd2c71d595e6f227cf18eb9ce7536966aa52b
Tal Rofe <tal@exlint.io>
2022-05-17 19:29:35 +0300

    refactor: 💡 add backend repo
    add backend repo

---
+++ apps/backend/.eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	root: true,
+	extends: ['../../.eslintrc.cjs'],
+	parserOptions: {
+		ecmaVersion: 12,
+		project: './tsconfig.eslint.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+		extraFileExtensions: ['.cjs'],
+	},
+};
