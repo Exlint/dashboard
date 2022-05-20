03d84f1a4d374fa94a4a05b2875ba2196b5c1bc7
Tal Rofe <tal@exlint.io>
2022-05-20 14:12:53 +0300

    refactor: 💡 apply stylelint linter
    apply stylelint linter

--- inflint.config.ts
+++ inflint.config.ts
@@ -4,6 +4,7 @@ const inflintConfig: Config = {
 	rules: {
 		'scripts/**/*': [2, 'kebab-case'],
 		'**/*.yml': 2,
+		'apps/frontend/**/*.css': 2,
 	},
 };
 
--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
-	'apps/backend/**/*.{ts,js,cjs}': ['eslint -c ./apps/backend/.eslintrc.cjs --fix', () => 'tsc --noEmit'],
-	'apps/frontend/**/*.{ts,js,cjs}': ['eslint -c ./apps/frontend/.eslintrc.cjs --fix', () => 'tsc --noEmit'],
+	'apps/backend/**/*.{ts,js,cjs}': [
+		'eslint -c ./apps/backend/.eslintrc.cjs --ignore-path ./apps/backend/.eslintignore --fix',
+		() => 'tsc --noEmit',
+	],
+	'apps/frontend/**/*.{ts,js,cjs}': [
+		'eslint -c ./apps/frontend/.eslintrc.cjs --ignore-path ./apps/frontend/.eslintignore --fix',
+		() => 'tsc --noEmit',
+	],
 	'!(apps)**/*.{ts,js,cjs}': ['eslint -c ./.eslintrc.cjs --fix', () => 'tsc --noEmit'],
+	'apps/frontend/src/**/*.scss': 'stylelint --config ./apps/frontend/stylelint.config.cjs --fix',
 	'**/*.{ts,js,cjs,json,yaml}': 'prettier --write',
 	'**/*': 'inflint -c ./inflint.config.ts',
 };
