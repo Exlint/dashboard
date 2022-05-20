554b88f23ef85add3898f7c5d3cb033acd7de242
Tal Rofe <tal@exlint.io>
2022-05-20 14:18:11 +0300

    refactor: 💡 more inflint enforcement
    more inflint enforcement

--- inflint.config.ts
+++ inflint.config.ts
@@ -2,9 +2,10 @@ import { Config } from 'inflint';
 
 const inflintConfig: Config = {
 	rules: {
-		'scripts/**/*': [2, 'kebab-case'],
+		'{scripts,assets}/**/*': [2, 'kebab-case'],
 		'**/*.yml': 2,
 		'apps/frontend/**/*.css': 2,
+		'apps/backend/src/**/*': [2, 'kebab-case.point'],
 	},
 };
 
