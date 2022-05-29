cf2d78efd2061d1b60ba04b905e73df2d51cc802
Tal Rofe <tal@exlint.io>
2022-05-29 19:50:20 +0300

    refactor: 💡 remove redundant done callback from validate google strategy
    remove redundant done callback from validate google strategy

--- apps/backend/src/modules/user/modules/auth/strategies/google.strategy.ts
+++ apps/backend/src/modules/user/modules/auth/strategies/google.strategy.ts
@@ -1,11 +1,11 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Strategy } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { IEnvironment } from '@/config/env.interface';
 
-import { IGoogleProfile } from '../interfaces/google';
+import { IGoogleProfile, IGoogleUser } from '../interfaces/google';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
@@ -18,12 +18,10 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 		});
 	}
 
-	validate(_: string, __: string, profile: IGoogleProfile, done: VerifyCallback): void {
-		const data = {
+	validate(_: string, __: string, profile: IGoogleProfile): IGoogleUser {
+		return {
 			email: profile.emails[0]!.value,
 			name: `${profile.name.givenName} ${profile.name.familyName}`,
 		};
-
-		done(null, data);
 	}
 }
