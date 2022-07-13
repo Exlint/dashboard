9c79072488d4954c811463724ddbe1d8f017147e
Tal Rofe <tal@exlint.io>
2022-07-13 21:32:59 +0300

    refactor: 💡 update inflint config
    update inflint config

--- inflint.config.ts
+++ inflint.config.ts
@@ -6,6 +6,8 @@ const inflintConfig: Config = {
 		'**/*.yml': 2,
 		'apps/frontend/**/*.css': 2,
 		'apps/backend/src/**/*': [2, 'kebab-case.point'],
+		'apps/frontend/src/{assets,data,hooks,i18n,interfaces,store,utils,styles}/**/*': [2, 'kebab-case'],
+		'apps/frontend/src/{components,pages}/**/*': [2, 'PascalCase.point'],
 	},
 };
 
