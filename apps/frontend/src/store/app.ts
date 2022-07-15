a5d093d999a7cdd95dd148ed89c8c2ef9d7fcebb
Tal Rofe <tal@exlint.io>
2022-07-15 12:33:24 +0300

    refactor: 💡 apply logout saga
    apply logout saga

--- apps/frontend/src/store/app.ts
+++ apps/frontend/src/store/app.ts
@@ -1,16 +1,22 @@
 import { createStore, Reducer, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import * as groupActions from './actions/groups';
 import * as fromGroups from './reducers/groups';
 import * as userActions from './actions/user';
 import * as fromUser from './reducers/user';
+import { watchUser } from './sagas/user';
 
 type reducerTypes = groupActions.GroupsTypes | userActions.UserTypes;
+
+const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer: Reducer<AppState, reducerTypes> = combineReducers({
 	groups: fromGroups.reducer,
 	user: fromUser.reducer,
 });
+
+sagaMiddleware.run(watchUser);
 
 export interface AppState {
 	groups: fromGroups.State;
