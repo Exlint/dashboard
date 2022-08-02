37beda4a5b92f66f2cbedfec2115c8fe82a2f78e
tal-rofe <tal@exlint.io>
2022-08-02 15:26:06 +0300

    refactor: 💡 new data structure for libraries
    new data structure for libraries

--- apps/frontend/src/data/libraries-list.ts
+++
@@ -1,7 +0,0 @@
-import { ESLint } from './eslint-rules';
-import { Stylelint } from './stylelint-rules';
-
-export const librariesList = {
-	ESLint,
-	Stylelint,
-};
---
+++ apps/frontend/src/interfaces/libraries.ts
@@ -0,0 +1,7 @@
+export interface ILbirariesData {
+	readonly eslint: Record<string, unknown>;
+	readonly depcheck: Record<string, unknown>;
+	readonly inflint: Record<string, unknown>;
+	readonly prettier: Record<string, unknown>;
+	readonly stylelint: Record<string, unknown>;
+}
