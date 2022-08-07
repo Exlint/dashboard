ae3d270e267e4f84761a4fcdecf90a41caf9996a
daniel <daniel45001@gmail.com>
2022-08-07 22:47:34 +0300

    refactor: 💡 Configed lint-staged file

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
 	'apps/backend/**/*.{ts,js,cjs}': [
-		'./apps/backend/node_modules/.bin/eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+		'./apps/backend/node_modules/.bin/eslint -c ./apps/backend/.eslintrc.cjs --ignore-path ./apps/backend/.eslintignore --fix',
 		() => 'tsc --noEmit',
 	],
 	'apps/cli-backend/**/*.{ts,js,cjs}': [
-		'./apps/cli-backend/node_modules/.bin/eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+		'./apps/cli-backend/node_modules/.bin/eslint -c ./apps/cli-backend/.eslintrc.cjs --ignore-path ./apps/cli-backend/.eslintignore --fix',
 		() => 'tsc --noEmit',
 	],
 	'apps/frontend/**/*.{ts,js,cjs}': [
-		'./apps/frontend/node_modules/.bin/eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+		'./apps/frontend/node_modules/.bin/eslint -c ./apps/frontend/.eslintrc.cjs --ignore-path ./apps/frontend/.eslintignore --fix',
 		() => 'tsc --noEmit',
 	],
 	'!(apps)**/*.{ts,js,cjs}': ['eslint -c ./.eslintrc.cjs --fix', () => 'tsc --noEmit'],
