b8fc8a9f9f1ef4a17a2b87ca34291d47a81a8e21
tal-rofe <tal@exlint.io>
2023-01-17 19:00:52 +0200

    ci: 🚀 partial Terraform
    partial Terraform

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -9,44 +9,6 @@ resource "aws_route53_zone" "primary" {
   )
 }
 
-data "kubernetes_service" "backend" {
-  metadata {
-    name = "backend-service"
-  }
-}
-
-data "kubernetes_service" "cli-backend" {
-  metadata {
-    name = "cli-backend-service"
-  }
-}
-
-data "aws_elb_hosted_zone_id" "services_hosted_zone" {}
-
-resource "aws_route53_record" "backend_record" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.api"
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service.backend.status.0.load_balancer.0.ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.services_hosted_zone.id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "cli_backend_record" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.cli-api"
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service.cli-backend.status.0.load_balancer.0.ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.services_hosted_zone.id
-    evaluate_target_health = true
-  }
-}
-
 resource "aws_route53_record" "frontend_record" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.app"
