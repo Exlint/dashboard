fa79408193905ddd84bc0b420da35a1aa1b9afa8
tal-rofe <tal@exlint.io>
2023-03-21 10:51:36 +0200

    refactor: 💡 [EXL-158] force revalidate after mutation
    force revalidate after mutation

--- apps/frontend/src/hooks/use-backend.ts
+++ apps/frontend/src/hooks/use-backend.ts
@@ -1,11 +1,23 @@
-import useSWR from 'swr';
+import useSWR, { type KeyedMutator } from 'swr';
+import { useCallback } from 'react';
 
 import BackendService from '@/services/backend';
 
 const useBackend = <D, E = unknown>(path: string | null) => {
 	const { data, error, isLoading, mutate } = useSWR<D, E>(path, BackendService.get);
 
-	return { data, error, isLoading, mutate };
+	const postMutation: KeyedMutator<D> = useCallback(
+		async (mutationData, options) => {
+			const data = await mutate(mutationData, options);
+
+			await mutate();
+
+			return data;
+		},
+		[mutate],
+	);
+
+	return { data, error, isLoading, mutate: postMutation };
 };
 
 export default useBackend;
