093cbebce12fbab4c41790a9f3b7f4ff1edaed81
tal-rofe <tal@exlint.io>
2023-04-08 09:49:09 +0300

    docs: 📘 document the use backend custom hook
    document the use backend custom hook

--- apps/frontend/src/hooks/use-backend.ts
+++ apps/frontend/src/hooks/use-backend.ts
@@ -6,6 +6,11 @@ import BackendService from '@/services/backend';
 const useBackend = <D, E = unknown>(path: string | null) => {
 	const { data, error, isLoading, mutate } = useSWR<D, E>(path, BackendService.get);
 
+	/**
+	 * * Forced mutation is required for synchronization issues,
+	 * * as noted here: https://stackoverflow.com/a/75796537/9105207
+	 * * The second mutation call uses cache therefore not extra API call network is made
+	 */
 	const forcedMutation: KeyedMutator<D> = useCallback(
 		async (mutationData, options) => {
 			const data = await mutate(mutationData, options);
